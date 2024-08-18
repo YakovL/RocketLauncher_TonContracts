e88842c762e90f67bdf865bf57dd315a50ea9375
Yakov Litvin <Yakov.A.Litvin@gmail.com>
2024-08-18 17:17:53 +0300

    feature(pool): implement send_jetton helper

--- wrappers/Pool.compile.ts
+++ wrappers/Pool.compile.ts
@@ -3,6 +3,7 @@ import { CompilerConfig } from '@ton/blueprint';
 export const compile: CompilerConfig = {
     lang: 'func',
     targets: [
+        'contracts/jetton/op-codes.fc',
         'contracts/pool.fc',
     ],
 };
