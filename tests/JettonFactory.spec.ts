1cac8b9e03bf24a177c0d357c5dca82dca5861b0
Yakov Litvin <Yakov.A.Litvin@gmail.com>
2024-09-16 15:18:57 +0300

    infra(factory): implement tests of upgraded contract (same code)

--- tests/JettonFactory.spec.ts
+++ tests/JettonFactory.spec.ts
@@ -6,110 +6,137 @@ import { JettonMinter } from '../wrappers/JettonMinter';
 import { Pool } from '../wrappers/Pool';
 import '@ton/test-utils';
 
-describe('JettonFactory', () => {
-    const metadataUri = 'https://github.com/YakovL/ton-example-jetton/raw/master/jetton-metadata.json';
+const config = {
+    upgrade_estimatedValue: 1000_000n, // fails for 500_000n
 
-    let factoryCode: Cell;
-    let minterCode: Cell;
-    let walletCode: Cell;
-    let poolCode: Cell;
-    beforeAll(async () => {
-        factoryCode = await compile('JettonFactory');
-        minterCode = await compile('JettonMinter');
-        walletCode = await compile('JettonWallet');
-        poolCode = await compile('Pool');
+    // normal values, far from edge cases
+    totalSupply: 100_000_000_000n,
+    minimalPrice: 1000_000n,
+};
+const metadataUri = 'https://github.com/YakovL/ton-example-jetton/raw/master/jetton-metadata.json';
+
+const getCompiledContracts = async () => {
+    return {
+        factoryCode: await compile('JettonFactory'),
+        minterCode: await compile('JettonMinter'),
+        walletCode: await compile('JettonWallet'),
+        poolCode: await compile('Pool'),
+    };
+};
+type CompiledContracts = Awaited<ReturnType<typeof getCompiledContracts>>;
+let compiledContractsCache: CompiledContracts | null = null;
+const getCompiledContractsWithCache = async () => {
+    compiledContractsCache = compiledContractsCache || await getCompiledContracts();
+    return compiledContractsCache;
+}
+
+const prepareTestEntities = async ({
+    factoryCode,
+    minterCode,
+    walletCode,
+    poolCode,
+}: CompiledContracts) => {
+    const blockchain = await Blockchain.create();
+    const deployer = await blockchain.treasury('deployer');
+    const nonDeployer = await blockchain.treasury('nonDeployer');
+
+    const jettonFactory = JettonFactory.createFromConfig({
+        minterCode,
+        walletCode,
+        poolCode,
+        adminAddress: deployer.address,
+        feePerMille: 10n,
+        maxDeployerSupplyPercent: 5n,
+    }, factoryCode);
+    const jettonFactoryContract = blockchain.openContract(jettonFactory);
+
+    const deployResult = await jettonFactoryContract.sendDeploy(
+        deployer.getSender(),
+        jettonFactoryContract.estimatedDeployGasPrice
+    );
+
+    expect(deployResult.transactions).toHaveTransaction({
+        from: deployer.address,
+        to: jettonFactoryContract.address,
+        deploy: true,
+        success: true,
     });
 
-    let blockchain: Blockchain;
-    let deployer: SandboxContract<TreasuryContract>;
-    let nonDeployer: SandboxContract<TreasuryContract>;
-    let jettonFactoryContract: SandboxContract<JettonFactory>;
-    beforeEach(async () => {
-        blockchain = await Blockchain.create();
-        deployer = await blockchain.treasury('deployer');
-        nonDeployer = await blockchain.treasury('nonDeployer');
+    return {
+        blockchain,
+        deployer,
+        nonDeployer,
+        jettonFactoryContract,
+    }
+}
+type TestContext = Awaited<ReturnType<typeof prepareTestEntities>>;
 
-        const jettonFactory = JettonFactory.createFromConfig({
-            minterCode,
-            walletCode,
-            poolCode,
-            adminAddress: deployer.address,
-            feePerMille: 10n,
-            maxDeployerSupplyPercent: 5n,
-        }, factoryCode);
-        jettonFactoryContract = blockchain.openContract(jettonFactory);
-
-        const deployResult = await jettonFactoryContract.sendDeploy(
-            deployer.getSender(),
-            jettonFactoryContract.estimatedDeployGasPrice
-        );
-
-        expect(deployResult.transactions).toHaveTransaction({
-            from: deployer.address,
-            to: jettonFactoryContract.address,
-            deploy: true,
-            success: true,
-        });
-    });
-
+const testFactoryFeatures = async (context : CompiledContracts & TestContext) => {
     it('should deploy', async () => {
         // the check is done inside beforeEach
         // blockchain and jettonFactoryContract are ready to use
+        expect(context.jettonFactoryContract).toBeTruthy();
     });
 
-    it('should deploy Pool', async () => {
-        const deployerSupplyPercent = await jettonFactoryContract.getMaxDeployerSupplyPercent();
+    it('should deploy Pool, Jetton, and mint it', async () => {
+        const deployerSupplyPercent = await context.jettonFactoryContract.getMaxDeployerSupplyPercent();
+        const { totalSupply, minimalPrice } = config;
     });
 
     it('should not deploy Pool when deployer requests too much supply share', async () => {
-        const deployerSupplyPercent = await jettonFactoryContract.getMaxDeployerSupplyPercent() + 1n;;
+        const deployerSupplyPercent = await context.jettonFactoryContract.getMaxDeployerSupplyPercent() + 1n;
+        const bigAmount = 100_000_000_000n;
 
-        const result = await jettonFactoryContract.sendInitiateNew(deployer.getSender(), 10_000_000n, {
+        const result = await context.jettonFactoryContract.sendInitiateNew(context.deployer.getSender(), bigAmount, {
             metadataUri,
             metadataType: 1,
-            totalSupply: 100_000_000_000n,
+            totalSupply: config.totalSupply,
             deployerSupplyPercent,
-            minimalPrice: 1000_000n,
+            minimalPrice: config.minimalPrice,
         });
+
         // see error_too_much_deployer_supply_share_requested
         expect(result.transactions).toHaveTransaction({ success: false, exitCode: 0xffa1 });
     });
 
-    // TODO: no longer valid, re-implement for the full chain
-    it.skip('should deploy Jetton', async () => {
-        const metadataUri = 'https://github.com/YakovL/ton-example-jetton/raw/master/jetton-metadata.json';
-        const deployResult = await jettonFactoryContract.sendDeployNewJetton(deployer.getSender(), {
-            totalSupply: 0n,
-            metadataType: 1,
-            metadataUri,
-        });
-
-        const minterAddress = JettonMinter.createFromConfig({
-            admin: jettonFactoryContract.address, // not deployer.address!
-            wallet_code: await compile('JettonWallet'),
-            content: JettonMinter.jettonContentToCell({ type: 1, uri: metadataUri }),
-        }, await compile('JettonMinter')).address;
-
-        expect(deployResult.transactions).toHaveTransaction({
-            from: jettonFactoryContract.address,
-            to: minterAddress,
-            oldStatus: 'uninitialized',
-            endStatus: 'active',
-        })
-    })
-
     it('should be upgradable by admin (deployer)', async () => {
-        const estimatedValue = 1000_000n; // fails for 500_000n
-        const result = await jettonFactoryContract.sendUpgrade(deployer.getSender(),
-            estimatedValue,
+        const result = await context.jettonFactoryContract.sendUpgrade(context.deployer.getSender(),
+            config.upgrade_estimatedValue,
             await compile('JettonFactory'));
         expect(result.transactions).not.toHaveTransaction({ success: false });
     });
     it('should not be upgradable by non-admin', async () => {
-        const estimatedValue = 1000_000n;
-        const result = await jettonFactoryContract.sendUpgrade(nonDeployer.getSender(),
-            estimatedValue,
+        const result = await context.jettonFactoryContract.sendUpgrade(context.nonDeployer.getSender(),
+            config.upgrade_estimatedValue,
             await compile('JettonFactory'));
         expect(result.transactions).toHaveTransaction({ success: false });
     });
+    // checks that the functionality is preserved are below (JettonFactory after upgrade)
+};
+
+describe('JettonFactory', () => {
+    const context = {} as CompiledContracts & TestContext;
+
+    beforeAll(async () => Object.assign(context, await getCompiledContractsWithCache()));
+
+    beforeEach(async () => Object.assign(context, await prepareTestEntities(context)));
+
+    testFactoryFeatures(context);
+});
+
+describe('JettonFactory after upgrade', () => {
+    const context = {} as CompiledContracts & TestContext;
+
+    beforeAll(async () => Object.assign(context, await getCompiledContractsWithCache()));
+
+    beforeEach(async () => {
+        Object.assign(context, await prepareTestEntities(context))
+
+        await context.jettonFactoryContract.sendUpgrade(context.deployer.getSender(),
+            config.upgrade_estimatedValue,
+            context.factoryCode // i.e. the same as before
+        );
+    });
+
+    testFactoryFeatures(context);
 });
