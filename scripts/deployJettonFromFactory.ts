6631aabe7ba4b5c32cb6f31676ed906c7ae8c438
Yakov Litvin <Yakov.A.Litvin@gmail.com>
2024-09-17 16:25:55 +0300

    infra: introduce mainnetConfig

--- scripts/deployJettonFromFactory.ts
+++ scripts/deployJettonFromFactory.ts
@@ -1,17 +1,17 @@
 import { JettonFactory } from '../wrappers/JettonFactory';
 import { NetworkProvider } from '@ton/blueprint';
 import { Address } from '@ton/core';
+import { mainnetConfig } from './mainnetConfig';
 
 export async function run(provider: NetworkProvider) {
     // TODO: ask for metadataUri, totalSupply, minimalPrice, and deployerSupplyPercent (and maybe factoryAddress) interactively instead
     throw 'update metadataUri, totalSupply, minimalPrice, and deployerSupplyPercent first'
-    const factoryAddress = "UQAKpTPWh6VT2raY3OCWgHPwI7HxyjF5Yc81NdlTVl8kOES0"; // upgradable
     const totalSupply = 0n;
     const deployerSupplyPercent = 0n;
     const minimalPrice = 1n;
     const metadataUri = '';
 
-    const jettonFactory = provider.open(JettonFactory.createFromAddress(Address.parse(factoryAddress)));
+    const jettonFactory = provider.open(JettonFactory.createFromAddress(Address.parse(mainnetConfig.factoryAddress)));
 
     await jettonFactory.sendInitiateNew(provider.sender(), 150_000_000n, {
         totalSupply,
---
+++ scripts/mainnetConfig.ts
@@ -0,0 +1,4 @@
+export const mainnetConfig = {
+    // upgradable
+    factoryAddress: 'UQAKpTPWh6VT2raY3OCWgHPwI7HxyjF5Yc81NdlTVl8kOES0'
+}
